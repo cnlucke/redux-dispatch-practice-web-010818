a97495672007b5ba0da364fce23b78e562a51276
cnlucke <cristy.lucke@gmail.com>
2018-03-29 12:17:45 -0400

    fixed bugs, still not working though

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,27 @@
 export let state;
 
-
-export function managePets(){
+export function managePets(state = {pets: []}, action = {type: '', pet: {}}){
+  switch (action.type) {
+    case 'ADD_PET':
+      return {...state, pets: [...state.pets, action.pet] }
+    case 'REMOVE_PET':
+      return {...state, pets: state.pets.filter(pet => pet != action.pet)}
+    default:
+      return state
+  }
 }
 
-export function dispatch(){
+export function dispatch(action){
+  state = managePets(state, action)
+  render()
 }
 
-export function render(){
-
+export function render() {
+  let container = document.getElementById('container')
+  const ul = document.createElement('ul')
+  const lis = state.pets.map(pet => `<li>${pet.name}</li>`)
+  container.appendChild(ul)
+  ul.innerHTML = lis
 }
+
+dispatch('@@INIT')
